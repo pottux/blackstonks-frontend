4bc1616c52bd8701ab31675767ef6e3b1a2aab8f
otsohellman <otso.hellman@gmail.com>
2019-11-15 22:42:52 +0200

    add category component

--- src/Containers/FrontPage/index.js
+++ src/Containers/FrontPage/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { PrimaryButton, SecondaryButton } from '../../Components/Button';
+import CategoryList from '../../Components/CategoryList';
 import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 0px 48px;
+`
 
 const Header = styled.h2`
   font-family: Roboto;
@@ -12,15 +17,33 @@ const ButtonContainer = styled.div `
   margin-top: 20em;
 `
 
+const MOCK_CATEGORY_DATA = [
+  {
+    name: 'Netflix',
+    price: 16.99,
+    type: 'monthly'
+  },
+  {
+    name: 'Kalja',
+    price: 123.99,
+    type: 'monthly'
+  },
+  {
+    name: 'Spotify',
+    price: 12.99,
+    type: 'monthly'
+  }
+]
 
 const FrontPage = () => {
   return(
-    <div>
+    <Container>
       <Header>Stonks</Header>
       <ButtonContainer>
         <SecondaryButton title={'Spotify family'}/>
       </ButtonContainer>
-    </div>
+      <CategoryList categoryData={MOCK_CATEGORY_DATA}/>
+    </Container>
   )
 }
 
